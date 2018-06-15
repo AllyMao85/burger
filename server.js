3481e81456efd032a8d41c5742d860185fc7721d
Luqiang Mao <luqiangmao@Luqiangs-MacBook-Pro.local>
2018-06-14 19:47:31 -0700

    update gitignore file

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path=require("path");
+var PORT = process.env.PORT || 3400;
+
+var app = express();
+const favicon = require('express-favicon');
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+//app.use(favicon(__dirname + '/public/favicon.png'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+//app.set('views', path.join(__dirname, '/../views'));    
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
